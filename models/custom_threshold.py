9333614ae6b36ef1e5579bdec12a3ba9be522406
farizzs <fr0388060@gmail.com>
2024-03-19 11:10:18 +0530

    task completed

--- models/custom_threshold.py
+++ models/custom_threshold.py
@@ -13,6 +13,7 @@ class CustomSaleOrder(models.Model):
     _inherit = 'sale.order'
 
     approve_bool = fields.Boolean(string="approve bool")
+    check_bool = fields.Boolean(string="check_bool", compute="_compute_total_amount")
 
     def approve_fn(self):
         self.approve_bool = True
@@ -26,11 +27,24 @@ class CustomSaleOrder(models.Model):
         res = super(CustomSaleOrder, self).action_confirm()
         custom = float(self.env['ir.config_parameter'].sudo().get_param
                        ('sale.threshold_amount'))
+
         if self.amount_total < custom and self.approve_bool == True:
             self.action_confirm()
         elif self.amount_total > custom and self.approve_bool == False:
-            raise UserError("Approve needed or Rejected")
+            raise UserError("Approve needed for confirmation")
+        elif self.amount_total < custom:
+            self.check_bool = True
+        elif self.amount_total > custom:
+            self.check_bool = False
 
+    @api.depends('check_bool', 'amount_total')
+    def _compute_total_amount(self):
+        threshold = float(self.env['ir.config_parameter'].sudo().get_param
+                       ('sale.threshold_amount'))
+        if self.amount_total < threshold:
+            self.check_bool = True
+        else:
+            self.check_bool = False
 
         # elif self.approve_bool == False:
         #     res = super(CustomSaleOrder, self).action_cancel()
